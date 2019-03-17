a1503abed1e2488d87ffc3ec4624ac7e7e624718
koirand <koirand.jp@gmail.com>
2019-03-18 00:34:46 +0900

    Add Logo and GitHub Link #20

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,13 @@ class App extends Component {
   render () {
     return (
       <div className={styles.app} >
+        <header className={styles.header}>
+          <h1 className={styles.title}>nixie-timer</h1>
+          <i
+            className={'fab fa-github ' + styles.github}
+            onClick={() => { window.location.href = 'https://github.com/koirand/nixie-timer' }}
+          />
+        </header>
         <div>
           <Clock />
           <div className={styles.buttonContainer}>
