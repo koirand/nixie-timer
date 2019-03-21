8913b538abd1989d265589cb7f7ba20a7899bbc4
koirand <koirand.jp@gmail.com>
2019-03-21 17:42:17 +0900

    [ci skip]

--- precache-manifest.cdf23860eb869c3c1de5742b1b20af9a.js
+++ precache-manifest.cdf23860eb869c3c1de5742b1b20af9a.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/nixie-timer/static/js/runtime~main.e4c5998c.js"
   },
   {
-    "revision": "9529cf3ef67203946531",
+    "revision": "bf8dd7d108183336b66e",
     "url": "/nixie-timer/static/js/main.76c53a4c.chunk.js"
   },
   {
@@ -12,11 +12,11 @@ self.__precacheManifest = [
     "url": "/nixie-timer/static/js/2.98060204.chunk.js"
   },
   {
-    "revision": "9529cf3ef67203946531",
-    "url": "/nixie-timer/static/css/main.0d2855ff.chunk.css"
+    "revision": "bf8dd7d108183336b66e",
+    "url": "/nixie-timer/static/css/main.f86c7406.chunk.css"
   },
   {
-    "revision": "796a88f864de0fe4384f9ab5dfc9b704",
+    "revision": "30af48ede8f34e337f82fe07274ee02e",
     "url": "/nixie-timer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nixie-timer/precache-manifest.e4bf1d6fec1c2026bc763fe33bbf4803.js"
+  "/nixie-timer/precache-manifest.cdf23860eb869c3c1de5742b1b20af9a.js"
 );
 
 workbox.clientsClaim();
